3c7494f4f47be04973c7811726e41dc70136d52c
webilop <webilop@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-24 15:22:46 +0000

    About section was added to settings page
    git-svn-id: https://plugins.svn.wordpress.org/user-language-switch/trunk@745723 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- user-language-switch.php
+++ user-language-switch.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: User Language Switch
-Description: ULS allows backend users to set the language displayed in the back-end and front-end of your site. It also allows to translate pages and posts.
+Description: Allows backend users to set the language displayed in the back-end and front-end of your site. It also allows to translate pages and posts.
 Version: 1.0
 Author: webilop
 Author URI: www.webilop.com
